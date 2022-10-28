883bb3d3a7ad921988325bc31bc015cbca74be81
doctorpack <enricopachera@gmail.com>
2022-10-28 09:59:13 +0200

    end excercise

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const arrGroceryList = [
+    'sugo',
+    'cracker',
+    'tonno rio mare',
+]
+
+
+const groceryList = document.querySelector('.grocery-list');
+const form = document.querySelector('form');
+const message = document.querySelector('.alert-message');
+const text = document.querySelector('.text');
+
+
+let i = 0
+
+
+while (i < arrGroceryList.length) {
+
+    groceryList.innerHTML += `<li>${arrGroceryList[i]}</li>`;
+
+    i++;
+
+}
+
+
+
+form.addEventListener('submit', function(event) {
+
+    event.preventDefault()
+
+    if (arrGroceryList.length >= 10) {
+        message.innerHTML = 'limit max'
+        text.value = ''
+        message.classList.add('negative')
+        message.classList.remove('empty')
+
+    } else if (text.value == '') {
+        message.innerHTML = 'add item'
+        message.classList.add('empty')
+        message.classList.remove('negative')
+    } else {
+        arrGroceryList.push(text.value);
+        groceryList.innerHTML += `<li>${text.value}</li>`
+        message.innerHTML = ''
+        text.value = '' 
+        message.classList.remove('empty')
+        message.classList.remove('negative')
+    }
+});
